11450a9d0e034a08308785a76df519bfd9403aa1
Aktan <you@example.com>
2024-02-29 13:19:52 +0300

    1

---
+++ bin/brain-progression.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+import { generateRandomNumber, greetings } from '../src/cli.js';
--- src/cli.js
+++ src/cli.js
@@ -4,40 +4,36 @@ const greetings = () => {
   console.log('Welcome to the Brain');
   const name = readlinesync.question('What is your name? ');
   console.log(`Hello,${name}!`);
-  return name
+  return name;
 };
 
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 //
 
 // Функции для НОК'а
-const findGDC = (a,b) => {
-    while(b !== 0){
-        let temp = b
-        b = a % b 
-         a = temp
-    }
-    return a
-}
+const findGDC = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
 
+const findLDC = (a, b) => (a * b) / findGDC(a, b);
 
-const findLDC = (a,b) => {
-    return a * b / findGDC(a, b)
-}
-
-
-const findLowestThreeNumbers = (a,b,c) => {
-    let lcmAB = findLDC(a,b)
-    let lcmBC = findLDC(b,c)
-    let lcABC = findLDC(lcmAB,lcmBC)
-    return lcABC
-}
+const findLowestThreeNumbers = (a, b, c) => {
+  const lcmAB = findLDC(a, b);
+  const lcmBC = findLDC(b, c);
+  const lcABC = findLDC(lcmAB, lcmBC);
+  return lcABC;
+};
 //
 
 // Функции для прогрессии
 
-
-
 //
 
-export { greetings, generateRandomNumber, findGDC, findLDC,findLowestThreeNumbers };
+export {
+  greetings, generateRandomNumber, findGDC, findLDC, findLowestThreeNumbers,
+};
